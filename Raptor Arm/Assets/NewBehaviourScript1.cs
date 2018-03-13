d9273bf1cab95c2299f490d894e702c6f2c3d0ff
Whirlwind348 <36898917+Whirlwind348@users.noreply.github.com>
2018-03-13 11:08:02 -0400

    IN Progress

---
+++ Raptor Arm/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+	public AudioSource Fire;
+	public AudioClip CLip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while (Input.GetKey("q")){
+			Fire.Play();
+		}
+			
+		
+	}
+}
